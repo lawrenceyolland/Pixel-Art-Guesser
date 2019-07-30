1a43c83e397cc587d3b33f23bc1756f416d9f1a6
github username <github email address>
2019-07-30 16:32:06 +0100

    reformatted source for color picker

--- front_end/index.js
+++ front_end/index.js
@@ -49,11 +49,16 @@ const makeArt = () => {
     );
   };
 
+
   const showColorPalette = () => {
     colorPalette.style.display = "block";
-    let newDiv =
-      '<div class="color-option" style="background-color:#000000" ></div><div class="color-option" style="background-color:#191919" ></div><div class="color-option" style="background-color:#323232" ></div><div class="color-option" style="background-color:#4b4b4b" ></div><div class="color-option" style="background-color:#646464" ></div><div class="color-option" style="background-color:#7d7d7d" ></div><div class="color-option" style="background-color:#969696" ></div><div class="color-option" style="background-color:#afafaf" ></div><div class="color-option" style="background-color:#c8c8c8" ></div><div class="color-option" style="background-color:#e1e1e1" ></div><div class="color-option" style="background-color:#ffffff" ></div><div class="color-option" style="background-color:#820000" ></div><div class="color-option" style="background-color:#9b0000" ></div><div class="color-option" style="background-color:#b40000" ></div><div class="color-option" style="background-color:#cd0000" ></div><div class="color-option" style="background-color:#e60000" ></div><div class="color-option" style="background-color:#ff0000" ></div><div class="color-option" style="background-color:#ff1919" ></div><div class="color-option" style="background-color:#ff3232" ></div><div class="color-option" style="background-color:#ff4b4b" ></div><div class="color-option" style="background-color:#ff6464" ></div><div class="color-option" style="background-color:#ff7d7d" ></div><div class="color-option" style="background-color:#823400" ></div><div class="color-option" style="background-color:#9b3e00" ></div><div class="color-option" style="background-color:#b44800" ></div><div class="color-option" style="background-color:#cd5200" ></div><div class="color-option" style="background-color:#e65c00" ></div><div class="color-option" style="background-color:#ff6600" ></div><div class="color-option" style="background-color:#ff7519" ></div><div class="color-option" style="background-color:#ff8532" ></div><div class="color-option" style="background-color:#ff944b" ></div><div class="color-option" style="background-color:#ffa364" ></div><div class="color-option" style="background-color:#ffb27d" ></div><div class="color-option" style="background-color:#828200" ></div><div class="color-option" style="background-color:#9b9b00" ></div><div class="color-option" style="background-color:#b4b400" ></div><div class="color-option" style="background-color:#cdcd00" ></div><div class="color-option" style="background-color:#e6e600" ></div><div class="color-option" style="background-color:#ffff00" ></div><div class="color-option" style="background-color:#ffff19" ></div><div class="color-option" style="background-color:#ffff32" ></div><div class="color-option" style="background-color:#ffff4b" ></div><div class="color-option" style="background-color:#ffff64" ></div><div class="color-option" style="background-color:#ffff7d" ></div><div class="color-option" style="background-color:#003300" ></div><div class="color-option" style="background-color:#004d00" ></div><div class="color-option" style="background-color:#008000" ></div><div class="color-option" style="background-color:#00b300" ></div><div class="color-option" style="background-color:#00cc00" ></div><div class="color-option" style="background-color:#00e600" ></div><div class="color-option" style="background-color:#1aff1a" ></div><div class="color-option" style="background-color:#4dff4d" ></div><div class="color-option" style="background-color:#66ff66" ></div><div class="color-option" style="background-color:#80ff80" ></div><div class="color-option" style="background-color:#b3ffb3" ></div><div class="color-option" style="background-color:#001a4d" ></div><div class="color-option" style="background-color:#002b80" ></div><div class="color-option" style="background-color:#003cb3" ></div><div class="color-option" style="background-color:#004de6" ></div><div class="color-option" style="background-color:#0000ff" ></div><div class="color-option" style="background-color:#0055ff" ></div><div class="color-option" style="background-color:#3377ff" ></div><div class="color-option" style="background-color:#4d88ff" ></div><div class="color-option" style="background-color:#6699ff" ></div><div class="color-option" style="background-color:#80b3ff" ></div><div class="color-option" style="background-color:#b3d1ff" ></div><div class="color-option" style="background-color:#003333" ></div><div class="color-option" style="background-color:#004d4d" ></div><div class="color-option" style="background-color:#006666" ></div><div class="color-option" style="background-color:#009999" ></div><div class="color-option" style="background-color:#00cccc" ></div><div class="color-option" style="background-color:#00ffff" ></div><div class="color-option" style="background-color:#1affff" ></div><div class="color-option" style="background-color:#33ffff" ></div><div class="color-option" style="background-color:#4dffff" ></div><div class="color-option" style="background-color:#80ffff" ></div><div class="color-option" style="background-color:#b3ffff" ></div><div class="color-option" style="background-color:#4d004d" ></div><div class="color-option" style="background-color:#602060" ></div><div class="color-option" style="background-color:#660066" ></div><div class="color-option" style="background-color:#993399" ></div><div class="color-option" style="background-color:#ac39ac" ></div><div class="color-option" style="background-color:#bf40bf" ></div><div class="color-option" style="background-color:#c653c6" ></div><div class="color-option" style="background-color:#cc66cc" ></div><div class="color-option" style="background-color:#d279d2" ></div><div class="color-option" style="background-color:#d98cd9" ></div><div class="color-option" style="background-color:#df9fdf" ></div><div class="color-option" style="background-color:#660029" ></div><div class="color-option" style="background-color:#800033" ></div><div class="color-option" style="background-color:#b30047" ></div><div class="color-option" style="background-color:#cc0052" ></div><div class="color-option" style="background-color:#e6005c" ></div><div class="color-option" style="background-color:#ff0066" ></div><div class="color-option" style="background-color:#ff1a75" ></div><div class="color-option" style="background-color:#ff3385" ></div><div class="color-option" style="background-color:#ff4d94" ></div><div class="color-option" style="background-color:#ff66a3" ></div><div class="color-option" style="background-color:#ff99c2" ></div>';
-    colorPalette.innerHTML = newDiv;
+    const swatches = ["#000000", "#191919", "#323232", "#4b4b4b", "#646464", "#7d7d7d", "#969696", "#afafaf", "#c8c8c8", "#e1e1e1", "#ffffff", "#820000", "#9b0000", "#b40000", "#cd0000", "#e60000", "#ff0000", "#ff1919", "#ff3232", "#ff4b4b", "#ff6464", "#ff7d7d", "#823400", "#9b3e00", "#b44800", "#cd5200", "#e65c00", "#ff6600", "#ff7519", "#ff8532", "#ff944b", "#ffa364", "#ffb27d", "#828200", "#9b9b00", "#b4b400", "#cdcd00", "#e6e600", "#ffff00", "#ffff19", "#ffff32", "#ffff4b", "#ffff64", "#ffff7d", "#003300", "#004d00", "#008000", "#00b300", "#00cc00", "#00e600", "#1aff1a", "#4dff4d", "#66ff66", "#80ff80", "#b3ffb3", "#001a4d", "#002b80", "#003cb3", "#004de6", "#0000ff", "#0055ff", "#3377ff", "#4d88ff", "#6699ff", "#80b3ff", "#b3d1ff", "#003333", "#004d4d", "#006666", "#009999", "#00cccc", "#00ffff", "#1affff", "#33ffff", "#4dffff", "#80ffff", "#b3ffff", "#4d004d", "#602060", "#660066", "#993399", "#ac39ac", "#bf40bf", "#c653c6", "#cc66cc", "#d279d2", "#d98cd9", "#df9fdf", "#660029", "#800033", "#b30047", "#cc0052", "#e6005c", "#ff0066", "#ff1a75", "#ff3385", "#ff4d94", "#ff66a3", "#ff99c2" ];
+    for (let i = 0; i<swatches.length; i++){
+      let div = document.createElement("div")
+      div.setAttribute("class", "color-option")
+      div.setAttribute("style", `background-color: ${swatches[i]}`);
+      colorPalette.append(div)
+    }
     const colorOption = document.querySelectorAll(".color-option");
     colorOption.forEach(o => o.addEventListener("click", chooseColor));
   };
@@ -73,13 +78,8 @@ const makeArt = () => {
   colorPalette.mouseIsOver = false;
   colorInput.style.borderRight = `10px solid ${colorInput.value}`;
 
-  colorPalette.onmouseover = () => {
-    colorPalette.mouseIsOver = true;
-  };
-
-  colorPalette.onmouseout = () => {
-    colorPalette.mouseIsOver = false;
-  };
+  colorPalette.onmouseover = () => (colorPalette.mouseIsOver = true);
+  colorPalette.onmouseout = () => (colorPalette.mouseIsOver = false);
 
   const hoverActiveColor = e => {
     e.target.style.boxShadow =
@@ -88,13 +88,9 @@ const makeArt = () => {
       !colorInput.value === true ? "not-allowed" : "crosshair";
   };
 
-  const hoverInactiveColor = e => {
-    e.target.style.boxShadow = "";
-  };
+  const hoverInactiveColor = e => (e.target.style.boxShadow = "");
 
-  const changeColor = e => {
-    e.target.style.backgroundColor = colorInput.value;
-  };
+  const changeColor = e => (e.target.style.backgroundColor = colorInput.value);
 
   const addRemoveColor = e => {
     if (e.target.style.backgroundColor === "") {
